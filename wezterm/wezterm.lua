9d18b065adaa67c6feedb7c5dbedaf091822bab5
youguanxinqing <youguanxinqing@qq.com>
2024-06-19 21:21:59 +0800

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local Config = require("config")
+
+-- require('events.right-status').setup()
+-- require('events.tab-title').setup()
+-- require('events.new-tab-button').setup()
+
+return Config:init()
+    :append(require("config.appearance"))
+    :append(require("config.bindings"))
+    :append(require("config.remote")).options
