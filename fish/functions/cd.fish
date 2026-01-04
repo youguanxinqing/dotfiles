e10f2e3b8c11f082877381ba30a3b9885b1f6ac6
youguanxinqing <youguanxinqing@qq.com>
2026-01-04 21:57:41 +0800

    support to cd -

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -1,10 +1,21 @@
 function cd
-  if set -q __direnv_done
-    builtin cd $argv
-  else
+  # Initialize direnv on first run
+  if not set -q __direnv_done
     # direnv: https://github.com/direnv/direnv
     direnv hook fish | source
     set -g __direnv_done 1
+  end
+
+  # Handle cd -
+  if test "$argv[1]" = "-"
+    if set -q OLDPWD
+      set -l target $OLDPWD
+      set -gx OLDPWD $PWD
+      builtin cd $target
+    end
+  else
+    # Save current directory before changing
+    set -gx OLDPWD $PWD
     builtin cd $argv
   end
 end
